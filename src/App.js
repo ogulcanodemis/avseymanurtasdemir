7595ca54e24ca2a22cdc3e63180f4506c0eccc78
ogulcanodemis <ogulcanodemis@as4digital.com>
2024-10-03 17:15:18 +0300

    faviconlar eklendi

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import React from 'react';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 import Home from './Pages/Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import KVKK from './Pages/kvkk';
@@ -9,6 +12,16 @@ function App() {
   return (
     <Router>
       <div className="App">
+        {/* WhatsApp Icon */}
+        <a
+          href="https://wa.me/905396867694"
+          className="whatsapp-float"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faWhatsapp} size="2x" />
+        </a>
+        
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/kvkk" element={<KVKK />} />
